47f4cd63caf4e9b4a043408b01d2625de03d0588
Shirin Pavel <trqktop@yandex.ru>
2022-10-12 14:21:20 +0300

    fetch/app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,16 +6,49 @@ import AppHeader from '../AppHeader/AppHeader';
 import BurgerConstructor from '../BurgerConstructor/BurgerConstructor';
 import BurgerIngredients from '../BurgerIngredients/BurgerIngredients';
 
-import data from '../../utils/data.js'
+// import data from '../../utils/data.js'
 
 function App() {
+  const [state, setState] = React.useState({
+    success: false,
+    data: {},
+    hasError: false,
+  })
+
+
+  const dataRequest = () => {
+    setState({ ...state, success: false, hasError: false })
+    fetch('https://norma.nomoreparties.space/api/ingredients')
+      .then(dataFromServer => dataFromServer.json())
+      .then(dataFromServer => setState({
+        success: true,
+        data: { ...dataFromServer },
+        hasError: false
+      }))
+      .catch(e => {
+        setState({ ...state, hasError: true, success: false });
+      });
+  }
+
+  React.useEffect(() => {
+    dataRequest()
+  }, [])
+
+  const { success, data } = { ...state }
+  console.log(data.data)
 
   return (
     <div className={appStyles.App}>
       <AppHeader />
       <main className={appStyles.main}>
-        <BurgerIngredients data={data} />
-        <BurgerConstructor data={data} />
+        {
+          state.success === true ? (
+            <>
+              {/* <BurgerIngredients data={data} />
+              <BurgerConstructor data={data} /> */}
+            </>
+          ) : <p>загрузка</p>
+        }
       </main>
       <footer></footer>
     </div>
